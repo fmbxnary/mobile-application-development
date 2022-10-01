9625014996e29bc71e8ca12996f4ce52fd3e47ec
Fatih Mehmet Baydar <baydarmf@gmail.com>
2022-10-01 20:54:45 +0300

    Update Animal.java

--- ListView/app/src/main/java/com/fmbxnary/mad_listview/Animal.java
+++ ListView/app/src/main/java/com/fmbxnary/mad_listview/Animal.java
@@ -4,24 +4,24 @@ public class Animal {
     private String type;
     private int picId;
 
-    public Animal (String type, int picId){
+    public Animal(String type, int picId) {
         this.type = type;
         this.picId = picId;
     }
 
-    public String getType(){
+    public String getType() {
         return type;
     }
 
-    public void setType(String type){
+    public void setType(String type) {
         this.type = type;
     }
 
-    public int getPicId(){
+    public int getPicId() {
         return picId;
     }
 
-    public void setPicId(int picId){
+    public void setPicId(int picId) {
         this.picId = picId;
     }
 }
