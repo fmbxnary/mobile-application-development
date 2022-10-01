c1a37b036ec3254a9ee2cf7814305662c7c08d8f
Fatih Mehmet Baydar <baydarmf@gmail.com>
2022-10-01 13:54:34 +0300

    Initial commit

---
+++ ListView/app/src/main/java/com/fmbxnary/mad_listview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.fmbxnary.mad_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnArrayAdapter, btnCustomAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnArrayAdapter = (Button) findViewById(R.id.btnArrayAdapter);
+        btnCustomAdapter = (Button) findViewById(R.id.btnCustomAdapter);
+        btnArrayAdapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ArrayAdapterActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnCustomAdapter.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, CustomAdapterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
